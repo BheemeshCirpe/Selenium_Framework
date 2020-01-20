d05a2c036192456319fca96150adab2322a09003
BheemeshCirpe <bheemesh.cirpe@gmail.com>
2020-01-20 23:52:16 +0530

    data driven script

--- src/test/java/com/makemytrip/qa/testcases/Flights_Page_Test.java
+++ src/test/java/com/makemytrip/qa/testcases/Flights_Page_Test.java
@@ -3,23 +3,32 @@ package com.makemytrip.qa.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import com.makemytrip.qa.base.TestBase;
 import com.makemytrip.qa.pages.Flights_Page;
 import com.makemytrip.qa.pages.Home_Page;
+import com.makemytrip.qa.util.TestUtil;
 
 public class Flights_Page_Test extends TestBase {
 
 	Home_Page homepage;
-
+	String sheetName = "Search_Flight";
 	@BeforeMethod
 	public void setUp() throws InterruptedException {
 		initialization();
 		homepage = new Home_Page();
 
 	}
+	
+	
+	@DataProvider()
+	public Object[][] getMakemyTripTestData() {
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}
 
-	@Test(priority=0,enabled = false)
+	@Test(priority=0)
 	public void navFilightsPage() {
 		homepage.navFlightPage();
 		Assert.assertEquals(homepage.verifyPageTitle(),
@@ -36,14 +45,14 @@ public class Flights_Page_Test extends TestBase {
 	}
 	
 	
-	@Test(priority=2)
-	public void searchSpecificFlight_2() throws InterruptedException {
-		homepage.navFlightPage().searchFlight_2("Chennai", "Paris");
+	@Test(priority=2,dataProvider ="getMakemyTripTestData" )
+	public void searchSpecificFlight_2(String fromCity,String ToCity) throws InterruptedException {
+		homepage.navFlightPage().searchFlight_2(fromCity, ToCity);
 	}
 	
 	
 	
-	/* @AfterMethod public void tearDown() { driver.quit(); } */
+	 @AfterMethod public void tearDown() { driver.quit(); } 
 	 
 
 }
